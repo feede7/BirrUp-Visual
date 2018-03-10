f6c8ce2f15ed042acc389e481881f7072e9a798b
Fede <feede.7@gmail.com>
2018-03-10 15:04:07 -0300

    Agrego manejo de tarjetas (la SUBE anda divino!)

--- BirrUp/Form1.Designer.cs
+++ BirrUp/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
@@ -40,6 +41,14 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox_saldo = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -163,11 +172,87 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(258, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(201, 226);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Card ID: ";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(314, 251);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Accion";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox_saldo
+            // 
+            this.textBox_saldo.Enabled = false;
+            this.textBox_saldo.Location = new System.Drawing.Point(251, 251);
+            this.textBox_saldo.Name = "textBox_saldo";
+            this.textBox_saldo.Size = new System.Drawing.Size(51, 20);
+            this.textBox_saldo.TabIndex = 20;
+            this.textBox_saldo.TextChanged += new System.EventHandler(this.textBox_saldo_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(202, 254);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Saldo $";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(336, 226);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 22;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(276, 277);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Remove";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(411, 312);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox_saldo);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -202,6 +287,14 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox_saldo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- BirrUp/Form1.cs
+++ BirrUp/Form1.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.NetworkInformation;
 using MySql.Data.MySqlClient;
-
+using System.IO.Ports;
 
 
 namespace BirrUp
@@ -20,11 +20,92 @@ namespace BirrUp
         Random Rand = new Random();
         byte[] AddrBytes = new byte[] { 192, 168, 0, 16 }; // byte array for server address.
         byte[] LocalhostBytes = new byte[] { 127, 0, 0, 1 }; // byte array for server address.
-        bool netOK = false;
 
         public Form1()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            if (check_comports() == "")
+            {
+                label1.Text = "Sin COM";
+                label1.ForeColor = Color.Coral;
+            }
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            if (indata.Length == 14)
+            {
+                present_card(indata.Replace("\r\n", string.Empty).Replace(" ", string.Empty));
+            }
+
+        }
+
+        private void present_card(string data) {
+            label6.Text = data;
+            label6.ForeColor = Color.Blue;
+            label9.ForeColor = Color.MediumVioletRed;
+            if (check_connection())
+            {
+                string aux_query = "SELECT * FROM Cards WHERE card LIKE '" + data + "'";
+                DataTable Table_aux = SQL_Query(aux_query);
+                if (Table_aux.Rows.Count == 0)
+                {
+                    button3.Text = "Add";
+                    label9.Text = "Sin registrar";
+                    button3.Enabled = true;
+                }
+                else
+                {
+                    button4.Enabled = true;
+                    label9.Text = "$" + Table_aux.Rows[0]["saldo"].ToString();
+                    string s = Table_aux.Rows[0]["enable"].ToString();
+                    if (Table_aux.Rows[0]["enable"].ToString() == "False")
+                    {
+                        button3.Text = "Enable";
+                        textBox_saldo.Enabled = true;
+                    }
+                    else
+                        button3.Text = "Disable";
+                    button3.Enabled = true;
+                }
+            }
+            else
+            {
+                label1.Text = "Sin server";
+                label1.ForeColor = Color.Coral;
+            }
+        }
+       
+        private string check_comports() {
+            SerialPort ComPort = new SerialPort();
+            string[] ArrayComPortsNames = null;
+
+            ArrayComPortsNames = SerialPort.GetPortNames();
+
+            if (ArrayComPortsNames.Length > 0)
+            {
+                try
+                {
+                    ComPort.PortName = ArrayComPortsNames[ArrayComPortsNames.Length-1];
+                    ComPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+                    ComPort.BaudRate = 115200;
+                    ComPort.Open();
+                    label1.Text = "COM abierto";
+                    label1.ForeColor = Color.LawnGreen;
+                    return ArrayComPortsNames[ArrayComPortsNames.Length-1];
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    label1.Text = "Error COM: " + ex.Message;
+                    label1.ForeColor = Color.Coral;
+                    return "Error";
+                }
+            }
+            return "";
         }
 
         private bool check_connection() {
@@ -78,8 +159,6 @@ namespace BirrUp
                         label1.ForeColor = Color.LawnGreen;
 
                         return table;
-
-                        //dataGridView1.DataSource = table;
                     }
                 }
                 else
@@ -174,6 +253,55 @@ namespace BirrUp
         {
             textBox_Available.BackColor = Color.White;
             label1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string aux_update = null;
+            int aux;
+            if (button3.Text != "Enable" || int.TryParse(textBox_saldo.Text, out aux))
+            {
+                switch (button3.Text)
+                {
+                    case "Add":
+                        aux_update = "INSERT INTO Cards VALUES('" + label6.Text.ToString() + "',0,0)";
+                        break;
+                    case "Enable":
+                        aux_update = "UPDATE Cards SET enable = 1,saldo = " + int.Parse(textBox_saldo.Text.ToString()) + " WHERE card LIKE '" + label6.Text.ToString() + "'";
+                        break;
+                    case "Disable":
+                        aux_update = "UPDATE Cards SET enable = 0,saldo = 0 WHERE card LIKE '" + label6.Text.ToString() + "'";
+                        break;
+                }
+                SQL_Query(aux_update);
+                label6.Text = "";
+                label9.Text = "";
+                button3.Text = "Accion";
+                textBox_saldo.Text = "";
+                textBox_saldo.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+            }
+            else
+                textBox_saldo.BackColor = Color.Coral;
+        }
+
+        private void textBox_saldo_TextChanged(object sender, EventArgs e)
+        {
+            textBox_saldo.BackColor = Color.White;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string aux_delete = "DELETE FROM Cards WHERE card LIKE '" + label6.Text.ToString() + "'";
+            SQL_Query(aux_delete);
+            label6.Text = "";
+            label9.Text = "";
+            button3.Text = "Accion";
+            textBox_saldo.Text = "";
+            textBox_saldo.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
         }
     }
 }
